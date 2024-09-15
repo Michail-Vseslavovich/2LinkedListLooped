99217cb0608f7985aa09ed01d1b6e78d3e506201
humo_r1st <miklvizh@gmail.com>
2024-09-15 22:18:17 +0300

    Добавьте файлы проекта.

---
+++ 2LinkedLoopedList.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace _2LinkedListLooped
+{
+    public class DoubleLinkedListLooped<T> : IEnumerable<T>
+    {
+        DoubleNode<T> Head;
+        int count;
+        public void Add(T Data)
+        {
+            DoubleNode<T> Node = new DoubleNode<T>(Data);
+            if (Head == null)
+            {
+                Head = Node;
+                Head.Previous = Head;
+                Head.Next = Node;
+            }
+            else
+            {
+                Node.Previous = Head;
+                Node.Next = Head;
+                Head.Previous.Next = Node;
+                Head.Previous = Node;
+            }
+
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            DoubleNode<T> current = Head;
+
+            DoubleNode<T> removedItem = null;
+            if (count == 0) return false;
+
+            // поиск удаляемого узла
+            do
+            {
+                if (current.Value.Equals(data))
+                {
+                    removedItem = current;
+                    break;
+                }
+                current = current.Next;
+            }
+            while (current != Head);
+
+            if (removedItem != null)
+            {
+                // если удаляется единственный элемент списка
+                if (count == 1)
+                    Head = null;
+                else
+                {
+                    // если удаляется первый элемент
+                    if (removedItem == Head)
+                    {
+                        Head = Head.Next;
+                    }
+                    removedItem.Previous.Next = removedItem.Next;
+                    removedItem.Next.Previous = removedItem.Previous;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            count = 0;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoubleNode<T> CurrentNode = Head;
+            do
+            {
+                if (CurrentNode != null)
+                {
+                    yield return CurrentNode.Next.Value;
+                }
+                else
+                {
+                    throw new Exception("Bezdarnost opredeli spisok a potom iterirui KURWA");
+                }
+            }
+            while (CurrentNode != Head);
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _2LinkedListLooped
+{
+    public class DoubleNode<T>
+    {
+        public T Value;
+        public DoubleNode<T> Previous;
+        public DoubleNode<T> Next;
+        public DoubleNode(T data)
+        {
+            Value = data;
+        }
+
+
+    }
+}
