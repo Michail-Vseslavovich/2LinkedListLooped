3ddf42a04505b55c9de5cc42e874e7520905c1fd
Michael Vizhlov <miklvizh@gmail.com>
2025-02-24 11:30:31 +0300

    added enumerator

---
+++ Code/LoopEnumerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2LinkedListLooped.Code
+{
+    internal class LoopEnumerator<T> : IEnumerator
+    {
+        private DoubleNode<T> current;
+        private DoubleNode<T> head;
+
+        public LoopEnumerator (DoubleNode<T> Node)
+        {
+            current = Node;
+            head = Node;
+        }
+
+        public bool MoveNext()
+        {
+            if (current.Next != head)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return current.Value;
+            }
+        }
+        public void Reset()
+        {
+            current = head;
+        }
+    }
+}
