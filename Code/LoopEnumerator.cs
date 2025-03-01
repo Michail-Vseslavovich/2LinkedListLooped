1aecdeca27ab80b21f314e9f51fa960ad9aa4982
Michael Vizhlov <miklvizh@gmail.com>
2025-03-01 22:17:31 +0300

    fixed enumerator

--- Code/LoopEnumerator.cs
+++ Code/LoopEnumerator.cs
@@ -5,25 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2LinkedListLooped.Code
+namespace LinkedListLooped.Code
 {
     internal class LoopEnumerator<T> : IEnumerator
     {
         private DoubleNode<T> current;
         private DoubleNode<T> head;
+        private bool questionNewEnum;
 
         public LoopEnumerator (DoubleNode<T> Node)
         {
             current = Node;
             head = Node;
+            questionNewEnum = true;
         }
 
         public bool MoveNext()
         {
-            if (current.Next != head)
+            if (questionNewEnum)
             {
+                questionNewEnum = false;
                 return true;
             }
+            if (current.Next != head)
+            {
+                
+                current = current.Next;
+                return true;
+            }
+            
+            
             return false;
         }
 
@@ -31,12 +42,14 @@ namespace _2LinkedListLooped.Code
         {
             get
             {
+                
                 return current.Value;
             }
         }
         public void Reset()
         {
             current = head;
+
         }
     }
 }
