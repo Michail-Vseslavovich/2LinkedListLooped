65f0050f3e583bf21fdb3e3fbafb3942da4b8678
Michael Vizhlov <miklvizh@gmail.com>
2025-03-02 20:46:05 +0300

    1

---
+++ Code/LoopEnumerator.cs
@@ -0,0 +1,56 @@
+﻿using _2LinkedListLooped;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListLooped.Code
+{
+    internal class LoopEnumerator<T> : IEnumerator
+    {
+        private DoubleNode<T> current;
+        private DoubleNode<T> head;
+        private bool questionNewEnum;
+
+        public LoopEnumerator(DoubleNode<T> Node)
+        {
+            current = Node;
+            head = Node;
+            questionNewEnum = true;
+        }
+
+        public bool MoveNext()
+        {
+            if (questionNewEnum)
+            {
+                questionNewEnum = false;
+                return true;
+            }
+            if (current.Next != head)
+            {
+
+                current = current.Next;
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public object Current
+        {
+            get
+            {
+
+                return current.Value;
+            }
+        }
+        public void Reset()
+        {
+            current = head;
+
+        }
+    }
+}
